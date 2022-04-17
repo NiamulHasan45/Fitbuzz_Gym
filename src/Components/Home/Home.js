91a9be8a81b967bf52748b4eff5153f63302024b
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-04-17 12:11:32 +0600

    banner-with-carousal

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import s1 from '../../images/slide-1-01.png';
+import s2 from '../../images/slide-2-01.png';
+import s3 from '../../images/slide-3-01.png';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home.</h2>
-        </div>
+      <div>
+            <Carousel fade>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={s1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Keep your body fit and strong with personal supervision</h3>
+                    <p>Health is the ultimate power of your strenth. So make it as strong as iron.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={s2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Unique meal can give you extra boost.</h3>
+                    <p>Food is the fuel of out body, so we offer individual food menue with your body type.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={s3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Feel the freshness of your mind.</h3>
+                    <p>Mind training can give you the power of positive thinking, so try to make good use of it.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+        <Services></Services>
+      </div>
     );
 };
 
--- src/Components/SharedComponents/Header/Header.js
+++ src/Components/SharedComponents/Header/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">Fitbuzz</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mx-auto">
-                        <Nav.Link className='mx-2' as={Link} to="home">Home</Nav.Link>
+                        <Nav.Link className='mx-2' as={Link} 
+                        to="home">Home</Nav.Link>
                         <Nav.Link className='mx-2' as={Link} to="about">About</Nav.Link>
                         <Nav.Link className='mx-2' as={Link} to="blogs">Blogs</Nav.Link>
                         <Nav.Link className='mx-2' as={Link} to="login">
