14e95376b204ed7ad78321423a1ac2d4ba73e8d9
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-04-18 13:06:58 +0600

    not-found

--- src/Components/NotFoud/NotFound.js
+++ src/Components/NotFoud/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h3>This is not found page</h3>
+        <div className='nf-container bg-dark d-flex justify-content-center align-items-center text-white'>
+            <h3>This page is not found page</h3>
         </div>
     );
 };
