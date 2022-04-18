7bdb7bbe679d4f1cae8306a4e252f4b93a2e172f
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-04-18 13:00:48 +0600

    all finished

--- src/Components/About/Service/Service.js
+++ src/Components/About/Service/Service.js
@@ -7,13 +7,15 @@ const Service = ({ service }) => {
     return (
         <div className='one-service-container p-3'>
             <img className='w-50' src={img} alt="" />
+            <div className='content'>
             <h2>{name}</h2>
-            <h3>Price: ${price}</h3>
+            <h3 className='text-primary'>Price: ${price}</h3>
             <p>Package Type: {duration}</p>
             <p className='mb-5'><small>{description}</small></p>
             <Link to='/checkout'>
                 <button className='service-button btn btn-primary d-block mx-auto'>{name}</button>
             </Link>
+            </div>
         </div>
     );
 };
